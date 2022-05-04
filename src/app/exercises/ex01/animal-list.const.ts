7fc80dd9d8ccc40b3d9cae5b5b6ac5093647c6aa
wiotrek <wiotrek@debian.wiotrek>
2022-05-04 18:01:18 +0200

    #3 - adding all animals

---
+++ src/app/exercises/ex01/animal-list.const.ts
@@ -0,0 +1,74 @@
+import {
+  faDog,
+  faFrog,
+  faBug,
+  faFish,
+  faHippo,
+  faHorse,
+  faCow,
+  faDragon,
+  faCrow,
+  faCat } from '@fortawesome/free-solid-svg-icons';
+
+export const AnimalListConst = [
+  {
+    id: 1,
+    animal: 'dog',
+    icon: faDog,
+    color: '#335C81'
+  },
+  {
+    id: 2,
+    animal: 'cat',
+    icon: faCat,
+    color: '#DFAEB4'
+  },
+  {
+    id: 3,
+    animal: 'frog',
+    icon: faFrog,
+    color: '#92DCE5'
+  },
+  {
+    id: 4,
+    animal: 'bug',
+    icon: faBug,
+    color: '#DB2763'
+  },
+  {
+    id: 5,
+    animal: 'fish',
+    icon: faFish,
+    color: '#B0DB43'
+  },
+  {
+    id: 6,
+    animal: 'hippo',
+    icon: faHippo,
+    color: '#FF5400'
+  },
+  {
+    id: 7,
+    animal: 'horse',
+    icon: faHorse,
+    color: '#9E0059'
+  },
+  {
+    id: 8,
+    animal: 'cow',
+    icon: faCow,
+    color: '#86A397'
+  },
+  {
+    id: 9,
+    animal: 'dragon',
+    icon: faDragon,
+    color: '#2B50AA'
+  },
+  {
+    id: 10,
+    animal: 'crow',
+    icon: faCrow,
+    color: '#04724D'
+  },
+];
--- src/app/exercises/ex01/ex01.component.ts
+++ src/app/exercises/ex01/ex01.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { faDog } from '@fortawesome/free-solid-svg-icons';
+import { Component } from '@angular/core';
+import { AnimalListConst } from "./animal-list.const";
 
 @Component({
   selector: 'app-ex01',
   templateUrl: './ex01.component.html',
   styleUrls: ['./ex01.component.scss']
 })
-export class Ex01Component implements OnInit {
-
-  faDog = faDog;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Ex01Component {
+  list = AnimalListConst;
 }
