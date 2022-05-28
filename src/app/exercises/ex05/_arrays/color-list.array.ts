197671746bc8e9b5ad24ea0976bbf494aab60ec6
wiotrek <wiotrek@debian.wiotrek>
2022-05-28 21:16:32 +0200

    #7 html is already

---
+++ src/app/exercises/ex05/_arrays/color-list.array.ts
@@ -0,0 +1,46 @@
+export const ColorListArray: {
+  id: number;
+  hex: string;
+  name: string;
+}[] = [
+  {
+    id: 0,
+    hex: '#ff0000',
+    name: 'red'
+  },
+  {
+    id: 1,
+    hex: '#1ce11c',
+    name: 'green'
+  },
+  {
+    id: 2,
+    hex: '#1414e5',
+    name: 'blue'
+  },
+  {
+    id: 3,
+    hex: '#ff9400',
+    name: 'orange'
+  },
+  {
+    id: 4,
+    hex: '#c069ff',
+    name: 'purple'
+  },
+  {
+    id: 5,
+    hex: '#ffe900',
+    name: 'yellow'
+  },
+  {
+    id: 6,
+    hex: '#b1b1b3',
+    name: 'grey'
+  },
+  {
+    id: 7,
+    hex: '#ff5733',
+    name: 'brown'
+  }
+];
--- src/app/exercises/ex05/ex05.component.ts
+++ src/app/exercises/ex05/ex05.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SummaryBtnType } from 'src/app/shared/components/summary/_types/summary-btn.type';
+import { ColorListArray } from 'src/app/exercises/ex05/_arrays/color-list.array';
 
 @Component({
   selector: 'app-ex05',
   templateUrl: './ex05.component.html',
   styleUrls: ['./ex05.component.scss']
 })
-export class Ex05Component implements OnInit {
+export class Ex05Component {
 
-  constructor() { }
+  /** === exercise default options */
+  isCheck = false;
+  result = 0;
+  amount = 0;
 
-  ngOnInit(): void {
+  list = ColorListArray;
+
+// catch events from bottom summary section
+  summaryEvents(btn: SummaryBtnType): void {
+    switch (btn) {
+      case "check":
+        break;
+      case "reset":
+        break;
+      case "next":
+        break;
+
+      default:
+        break;
+    }
   }
-
 }
